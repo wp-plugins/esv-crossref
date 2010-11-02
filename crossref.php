e4d72c1a492bef893d0b263ad7a97a9d2782113a
dwickwire <dwickwire@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-11-02 19:13:01 +0000

    Restructuring the repo
    git-svn-id: https://plugins.svn.wordpress.org/esv-crossref/tags/1.0@307065 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ crossref.php
@@ -0,0 +1,26 @@
+<?php
+/*
+Plugin Name: CrossRef
+Plugin URI: http://esv.org/tools/esv-crossref/
+Description: Scans the site for Bible references. Once it finds a Bible reference it will be converted into a hoverable link.
+Author: Crossway
+Version: 1.0
+Author URI: http://crossway.org/
+*/
+
+define('CROSSREF_VERSION', '1.0');
+
+class CrossRef 
+{
+	function loadScript () {
+		# loads the crossref script to the footer.
+        wp_enqueue_script('CrossRef', 'http://static.esvmedia.org/crossref/crossref.min.js', false, false, true);
+	}
+}
+
+//Enable the plugin for the init hook, but only if WP is loaded. 
+if (defined('ABSPATH') && defined('WPINC')) {
+	add_action("init",array("CrossRef","loadScript"),1, 0);
+}
+
+?>
