c8602757598208725515155a07fd0d59651a6717
dwickwire <dwickwire@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-01-07 18:25:56 +0000

    Updating code style, but really just making changes to make wordpress publicly list the plugin again.
    git-svn-id: https://plugins.svn.wordpress.org/esv-crossref/trunk@834474 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- crossref.php
+++ crossref.php
@@ -10,17 +10,17 @@ Author URI: http://crossway.org/
 
 define('CROSSREF_VERSION', '1.0');
 
-class CrossRef 
+class CrossRef
 {
 	function loadScript () {
 		# loads the crossref script to the footer.
-        wp_enqueue_script('CrossRef', 'http://static.esvmedia.org/crossref/crossref.min.js', false, false, true);
+    wp_enqueue_script('CrossRef', 'http://static.esvmedia.org/crossref/crossref.min.js', false, false, true);
 	}
 }
 
-//Enable the plugin for the init hook, but only if WP is loaded. 
+//Enable the plugin for the init hook, but only if WP is loaded.
 if (defined('ABSPATH') && defined('WPINC')) {
-	add_action("init",array("CrossRef","loadScript"),1, 0);
+	add_action("init", array("CrossRef","loadScript"), 1, 0);
 }
 
 ?>
